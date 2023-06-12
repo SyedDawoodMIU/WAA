67521224da555e35b8877e2f88a85641a0a368ba
Syed Dawood <syed.muhammaddawoodshah@miu.edu>
2023-06-12 15:07:48 -0500

    Detailed post implemented

--- react/assignments/src/components/Post.js
+++ react/assignments/src/components/Post.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "../components/Post.css";
 
-const Post = ({ post }) => {
+const Post = ({ post, deletePost, changePostDetails }) => {
+  const [toggleDetails, setToggleDetails] = React.useState(false);
+
   if (!post) {
     return <div>Post not found</div>;
   }
 
-  return (
-    <div className="post-block">
+  return toggleDetails ? (
+    <div onClick={() => setToggleDetails(!toggleDetails)}>
+      <h3>Id: {post.id}</h3>
+      <p>{post.details}</p>
+
+      <button onClick={() => deletePost(post.id)}>Delete</button>
+      <button onClick={() => changePostDetails(post)}>Edit</button>
+    </div>
+  ) : (
+    <div
+      className="post-block"
+      onClick={() => setToggleDetails(!toggleDetails)}
+    >
       <h3>Id: {post.id}</h3>
       <h1 className="post-title">Title: {post.title}</h1>
       <h2 className="post-author">Author: {post.author}</h2>
--- react/assignments/src/components/Posts.js
+++ react/assignments/src/components/Posts.js
@@ -2,28 +2,57 @@ import React from "react";
 import Post from "./Post";
 
 const Posts = ({ firstPostName }) => {
-  const Posts = [
+  const [Posts, setPosts] = React.useState([
     {
       id: 1,
       title: firstPostName ? firstPostName : "Macbook Pro",
       author: "Dawood",
+      details: "Post details",
     },
     {
       id: 2,
       title: "Macbook Air",
       author: "Dawood",
+      details: "Post details",
     },
     {
       id: 3,
       title: "Macbook Max",
       author: "Dawood",
+      details: "Post details",
     },
-  ];
+  ]);
+
+  function changePostDetails(newPost) {
+    const newPosts = Posts.map((post) => {
+      if (post.id === newPost.id) {
+        return {
+          ...post,
+          title: newPost.title,
+          author: newPost.author,
+          details: newPost.details,
+        };
+      }
+      return post;
+    });
+
+    setPosts(newPosts);
+  }
+
+  function deletePost(id) {
+    const newPosts = Posts.filter((post) => post.id !== id);
+    setPosts(newPosts);
+  }
 
   return (
     <div className="posts-block">
       {Posts.map((post) => (
-        <Post post={post} />
+        <Post
+          post={post}
+          key={post.id}
+          deletePost={deletePost}
+          changePostDetails={changePostDetails}
+        />
       ))}
     </div>
   );
